1a986e37a61267d9af033614f9f575d6477900d6
aaliboyaci <aaliboyaci@gmail.com>
2023-09-11 13:24:20 +0300

    configure layout of the app and make css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import { useQuery } from "@apollo/client";
 import { GET_COUNTRIES } from "./queries/getCountries";
-import Loading from "./components/Loading";
+import "./components/Loading.css";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   const { loading, error, data } = useQuery(GET_COUNTRIES);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  if (loading) return <Loading />;
+  console.log(data);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  if (loading)
+    return (
+      <div className="loading-container">
+        <div className="custom-loader"></div>
+      </div>
+    );
   if (error) return <p>Error: {error.message}</p>;
 
   const countries = data.countries;
 
   return (
-    <div>
-      <ul>
-        {countries.map((country: any) => (
-          <li key={country.code}>
-            {country.name} + {country.code}
-          </li>
-        ))}
-      </ul>
+    <div className="app-container">
+      <div className="text-filter">
+        <form className="search-form" onSubmit={handleSubmit}>
+          <input
+            className="search-input"
+            type="text"
+            value={searchTerm}
+            onChange={handleInputChange}
+            placeholder="Search for a country"
+          />
+        </form>
+      </div>
+      <div className="country-list">
+        <ul>
+          {countries.map((country: any) => (
+            <li key={country.code} className="country-item">
+              <div className="country-emoji">{country.emoji}</div>
+
+              <div className="country-info">
+                <div className="country-name">
+                  {country.name.substring(0, 30)}
+                  {country.name.length > 30 && <>...</>}
+                </div>
+                <div className="country-capital">{country.capital}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/queries/getCountries.ts
+++ src/queries/getCountries.ts
@@ -7,6 +7,7 @@ export const GET_COUNTRIES = gql`
       emoji
       currency
       code
+      capital
     }
   }
 `;
